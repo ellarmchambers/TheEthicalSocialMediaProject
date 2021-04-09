b54caa4f358b3fc10ccaad2512b1908fcfff0867
Ella Chambers <33899496+elloella@users.noreply.github.com>
2021-04-09 19:29:52 +0100

    Further improvements to Notification Page

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-function notifyMe(app){
-  var button = document.getElementById(app)
+function notifyMeOld(appold){ //function for old notifications
+  var button = document.getElementById(appold)
+  var audio = button.getElementsByTagName('audio')[0];
+  if (audio.paused) { //allows audio to restart when button is click
+    audio.play();
+  }else{
+    audio.currentTime = 0
+  }
+  var value = parseInt(button.getElementsByClassName('application-badge-old')[0].value); //increased the badge number on button click
+  value = isNaN(value) ? 0 : value;
+  value++;
+  button.getElementsByClassName('application-badge-old')[0].innerHTML = value; //printing value into HTML
+  button.getElementsByClassName('application-badge-old')[0].value = value; //storing the value
+  button.getElementsByClassName('application-badge-old')[0].style.display = 'block'; //shows badge on click
+}
+
+function notifyMeNew(appnew) { //function for new notifications
+  var button = document.getElementById(appnew)
   var audio = button.getElementsByTagName('audio')[0];
   if (audio.paused) {
     audio.play();
   }else{
     audio.currentTime = 0
   }
-  var value = parseInt(button.getElementsByClassName('application-badge')[0].value);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  button.getElementsByClassName('application-badge')[0].innerHTML = value;
-  button.getElementsByClassName('application-badge')[0].value = value;
+  button.getElementsByClassName('application-badge-new')[0].style.display = 'block'; //shows badge on click
 }
