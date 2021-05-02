4f906f28b203fc64b519281b126a4b4eb15e9203
Ella Chambers <33899496+elloella@users.noreply.github.com>
2021-05-02 12:12:13 +0100

    Adding more onscroll animations to Ethical

--- animate-ethical.js
+++ animate-ethical.js
@@ -1,13 +1,15 @@
 // On Scroll Animations
-var adoptionFeaturesAnimation = document.querySelector('#adoption-features-animation')
+var adoptionFeatures1Animation = document.querySelector('#adoption-features1-animation')
+var adoptionFeaturesTimeline1Animation = document.querySelector('#adoption-features-timeline1-animation')
+var adoptionFeatures2Animation = document.querySelector('#adoption-features2-animation')
+var adoptionFeaturesTimeline2Animation = document.querySelector('#adoption-features-timeline2-animation')
 var darkPatternsAnimation = document.querySelector('#dark-patterns-animation')
 var socialMediaAnalysisAnimation = document.querySelector('#social-media-analysis-animation')
-
-// var instagramAnimation = document.querySelector('#instagram-animation')
-// var snapchatAnimation = document.querySelector('#snapchat-animation')
-// var facebookAnimation = document.querySelector('#facebook-animation')
-// var productSocialMediaAnimation = document.querySelector('#product-social-media-animation')
-// var utopiaDystopiaAnimation = document.querySelector('#utopia-dystopia-animation')
+var instagramAnimation = document.querySelector('#instagram-animation')
+var snapchatAnimation = document.querySelector('#snapchat-animation')
+var facebookAnimation = document.querySelector('#facebook-animation')
+var productSocialMediaAnimation = document.querySelector('#product-social-media-animation')
+var utopiaDystopiaAnimation = document.querySelector('#utopia-dystopia-animation')
 
 function animateOnScroll(element){
   element.classList.remove('hide-row');
@@ -16,12 +18,28 @@ function animateOnScroll(element){
 
 window.onscroll = function() {
 
-  if (document.documentElement.scrollTop > 300 && document.documentElement.scrollTop < 1800) { 
-    animateOnScroll(adoptionFeaturesAnimation);
-  } else if (document.documentElement.scrollTop > 1800 && document.documentElement.scrollTop < 3000){
+  if (document.documentElement.scrollTop > 500 && document.documentElement.scrollTop < 800) { 
+    animateOnScroll(adoptionFeatures1Animation);
+  } else if (document.documentElement.scrollTop > 800 && document.documentElement.scrollTop < 1300){
+    animateOnScroll(adoptionFeaturesTimeline1Animation);
+  } else if (document.documentElement.scrollTop > 1300 && document.documentElement.scrollTop < 1500){
+    animateOnScroll(adoptionFeatures2Animation);
+  } else if (document.documentElement.scrollTop > 1500 && document.documentElement.scrollTop < 1900){
+    animateOnScroll(adoptionFeaturesTimeline2Animation);
+  } else if (document.documentElement.scrollTop > 1900 && document.documentElement.scrollTop < 3000){
     animateOnScroll(darkPatternsAnimation);
-  } else if (document.documentElement.scrollTop > 3000){
+  } else if (document.documentElement.scrollTop > 3000 && document.documentElement.scrollTop < 3300 ){
     animateOnScroll(socialMediaAnalysisAnimation);
+  } else if (document.documentElement.scrollTop > 3300 && document.documentElement.scrollTop < 3600){
+    animateOnScroll(instagramAnimation);
+  } else if (document.documentElement.scrollTop > 3600 && document.documentElement.scrollTop < 3900){
+    animateOnScroll(snapchatAnimation);
+  } else if (document.documentElement.scrollTop > 3900 && document.documentElement.scrollTop < 4300){
+    animateOnScroll(facebookAnimation);
+  } else if (document.documentElement.scrollTop > 4300 && document.documentElement.scrollTop < 5100){
+    animateOnScroll(productSocialMediaAnimation);
+  } else if (document.documentElement.scrollTop > 5100 ){
+    animateOnScroll(utopiaDystopiaAnimation);
   }
   
 
