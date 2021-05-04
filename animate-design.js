e8d0845335d25912abc39e18629b0c8b2d6892b2
Ella Chambers <33899496+elloella@users.noreply.github.com>
2021-05-04 16:38:39 +0100

    Changes to Nav Bar

--- animate-design.js
+++ animate-design.js
@@ -17,6 +17,7 @@ window.onscroll = function() {
   } else if (document.documentElement.scrollTop > 1400){
     animateOnScroll(featureThreeRow);
   }
+  
 //Nav Bar
 
   scrollFunction()}; //Setting up the scroll funtion for re-sizing of nav bar
@@ -25,11 +26,15 @@ window.onscroll = function() {
     document.querySelector(".nav-bar").style.backgroundColor = "#ffffff"; //Setting white background
     document.querySelector(".nav-design").style.padding = "20px 45px"; //Changing the ammount of padding on the nav items
     document.querySelector(".nav-ethical").style.padding = "20px 45px";
-    document.querySelector(".nav-logo").style.width = "60px"; //Changing width of the logo
+    document.querySelector(".nav-design").style.fontSize = "1.5em"; //Reducing the font size
+    document.querySelector(".nav-ethical").style.fontSize = "1.5em";
+    document.querySelector(".nav-logo").style.width = "55px"; //Changing width of the logo
   } else {
     document.querySelector(".nav-bar").style.backgroundColor = "transparent"; 
     document.querySelector(".nav-design").style.padding = "35px 45px";
     document.querySelector(".nav-ethical").style.padding = "35px 45px";
+    document.querySelector(".nav-design").style.fontSize = "1.8em";
+    document.querySelector(".nav-ethical").style.fontSize = "1.8em";
     document.querySelector(".nav-logo").style.width = "90px";
   }
 }
